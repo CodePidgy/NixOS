3f789b5418d1dddb5df0bee086418e4d72000766
Aidan Venter <aidanbv03@gmail.com>
2024-09-20 19:40:37 +0200

    Remove redundant comments

--- virtuos/nixos/modules/boot.nix
+++ virtuos/nixos/modules/boot.nix
@@ -2,8 +2,4 @@
 	boot.loader.grub.enable = true;
 	boot.loader.grub.device = "/dev/sda";
 	boot.loader.grub.useOSProber = true;
-	# boot.loader.systemd-boot.enable = true;
-	# boot.loader.efi.canTouchEfiVariables = true;
-	# boot.initrd.kernelModules = [ "amdgpu" ];
-	# boot.kernelParams = [ "psmouse.synaptics_intertouch=0" ];
 }
