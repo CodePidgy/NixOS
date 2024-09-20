3f52bf61aabe463cbc5323b164f3a8d404d958b9
Aidan Venter <aidanbv03@gmail.com>
2024-09-20 19:41:16 +0200

    Remove redundant comment

--- virtuos/nixos/modules/bluetooth.nix
+++ virtuos/nixos/modules/bluetooth.nix
@@ -3,14 +3,5 @@
 		enable = true;
 
 		powerOnBoot = false;
-
-		# settings = {
-		# 	General = {
-		# 		Enable = "Source,Sink,Media,Socket";
-		# 		Experimental = true;
-		# 	};
-		# };
 	};
-
-	# services.blueman.enable = true;
 }
