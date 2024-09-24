56cffcad0a8f3ca079c688625e7752e9e8e590e1
Aidan Venter <aidanbv03@gmail.com>
2024-09-24 15:26:45 +0200

    Remove redundant settings

--- home/virtuos/modules/thunderbird.nix
+++ home/virtuos/modules/thunderbird.nix
@@ -9,9 +9,6 @@
         };
 
         settings = {
-            "mailnews.default_sort_order" = 2;
-            "mailnews.default_sort_type" = 18;
-            "mailnews.default_view_flags" = 1;
             "mailnews.message_display.disable_remote_image" = false;
         };
     };
