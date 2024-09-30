b89656889d309dbfc46de8a2770ba8846e0c5f3f
Aidan Venter <aidanbv03@gmail.com>
2024-10-01 00:53:42 +0200

    Add animate o Dash to Panel

--- workbench/home/modules/dconf.nix
+++ workbench/home/modules/dconf.nix
@@ -2,6 +2,7 @@
     dconf = with lib.hm.gvariant; {
         settings = {
             "org/gnome/shell/extensions/dash-to-panel" = {
+                animate-appicon-hover = true;
                 available-monitors = [ 1 0 ];
                 panel-sizes = "{\"0\":32,\"1\":32}";
                 primary-monitor = 1;
