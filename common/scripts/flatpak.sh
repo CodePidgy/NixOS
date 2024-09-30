9cf89868749d6fbc39b47d7e8ca5edacd5e06271
Aidan Venter <aidanbv03@gmail.com>
2024-09-30 21:18:21 +0200

    Add Dropbox

--- common/scripts/flatpak.sh
+++ common/scripts/flatpak.sh
@@ -3,3 +3,4 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 flatpak install -y flathub com.axosoft.GitKraken
+flatpak install -y flathub com.dropbox.Client
