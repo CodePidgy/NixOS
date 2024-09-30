96df5292c1f36d153232107627c84aef0bdbeb0f
Aidan Venter <aidanbv03@gmail.com>
2024-09-30 16:44:04 +0200

    Fix home-manager erroring out with conflicting files

--- flake.nix
+++ flake.nix
@@ -49,6 +49,7 @@
 					./workbench/nixos/configuration.nix
 
 					home-manager.nixosModules.home-manager {
+						home-manager.backupFileExtension = "bak";
 						home-manager.useGlobalPkgs = true;
 						home-manager.useUserPackages = true;
 
