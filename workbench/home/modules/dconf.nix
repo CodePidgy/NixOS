2a2e467e583960e555275c39668e188395672073
Aidan Venter <aidanbv03@gmail.com>
2024-10-01 00:42:41 +0200

    Remove redundant panel-lengths

--- workbench/home/modules/dconf.nix
+++ workbench/home/modules/dconf.nix
@@ -3,7 +3,6 @@
         settings = {
             "org/gnome/shell/extensions/dash-to-panel" = {
                 available-monitors = [ 1 0 ];
-                panel-lengths = "{\"0\":100,\"1\":100}";
                 panel-sizes = "{\"0\":32,\"1\":32}";
                 primary-monitor = 1;
             };
