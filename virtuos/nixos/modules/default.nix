5831a5f10101b8b5eb1d8861f57debc355cd5508
Aidan Venter <aidanbv03@gmail.com>
2024-10-01 00:22:55 +0200

    Fix incorrect imports

--- virtuos/nixos/modules/default.nix
+++ virtuos/nixos/modules/default.nix
@@ -1,14 +1,6 @@
 {
 	imports = [
-		./bluetooth.nix
 		./boot.nix
-		./env.nix
-		./gnome.nix
-		./locale.nix
 		./networking.nix
-		./services.nix
-		./sound.nix
-		./user.nix
-		./xserver.nix
 	];
 }
