7eb713da350e93b49880e85b60f0146b304d3774
Aidan Venter <aidanbv03@gmail.com>
2024-10-06 11:46:37 +0200

    Reformat string

--- workbench/home/modules/dconf.nix
+++ workbench/home/modules/dconf.nix
@@ -19,7 +19,7 @@
             "org/gnome/shell/extensions/dash-to-panel" = {
                 animate-appicon-hover = true;
                 available-monitors = [ 1 0 ];
-                panel-sizes = "{\"0\":32,\"1\":32}";
+                panel-sizes = ''{ "0": 32, "1": 32 }'';
                 primary-monitor = 1;
             };
         };
