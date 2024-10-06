6e29bd90c6bec7e8447e800e37f906c92b554876
Aidan Venter <aidanbv03@gmail.com>
2024-10-06 11:25:47 +0200

    Add sensors to Astra Monitor

--- workbench/home/modules/dconf.nix
+++ workbench/home/modules/dconf.nix
@@ -1,6 +1,20 @@
 { lib, ... }: {
     dconf = with lib.hm.gvariant; {
         settings = {
+            "org/gnome/shell/extensions/astra-monitor" = {
+                sensors-header-show = true;
+                sensors-header-sensor1 = ''
+                    { "service": "hwmon", "path": [ "k10temp-{$15eb}", "Tctl", "input" ] }
+                '';
+                sensors-header-sensor2 = ''
+                    { "service": "hwmon", "path": [ "BAT1", "in0", "input" ] }
+                '';
+                sensors-header-sensor1-show = true;
+                sensors-header-sensor2-layout = "horizontal";
+                sensors-header-sensor2-show = true;
+                sensors-update = 2.0;
+            };
+
             "org/gnome/shell/extensions/dash-to-panel" = {
                 animate-appicon-hover = true;
                 available-monitors = [ 1 0 ];
