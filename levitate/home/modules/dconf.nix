d26ac9f8020ccd46d17d7a79afa18b7558d44297
Aidan Venter <aidanbv03@gmail.com>
2024-10-11 17:05:27 +0200

    Reformat settings

--- levitate/home/modules/dconf.nix
+++ levitate/home/modules/dconf.nix
@@ -2,9 +2,9 @@
     dconf = with lib.hm.gvariant; {
         settings = {
             "org/gnome/shell/extensions/dash-to-panel" = {
-                available-monitors = [0 1];
-                panel-lengths = "{\"0\":100,\"1\":100}";
-                panel-sizes = "{\"0\":32,\"1\":32}";
+                available-monitors = [ 0 1 ];
+                panel-lengths = ''{ "0": 100, "1": 100 }'';
+                panel-sizes = ''{ "0": 32, "1": 32 }'';
                 primary-monitor = 0;
             };
         };
