f4e5e09048ed9a4128431304792c98f718319720
Aidan Venter <aidanbv03@gmail.com>
2024-11-09 20:01:28 +0200

    Add rmbak service to help prevent home-manager from crashing

--- common/nixos/modules/services.nix
+++ common/nixos/modules/services.nix
@@ -1,3 +1,12 @@
 { pkgs, ... }: {
 	services.flatpak.enable = true;
+
+	systemd.services.rmbak = {
+		script = ''
+			if [ -f /home/aidan/.config/gtk-4.0/gtk.css.bak ]; then
+				rm /home/aidan/.config/gtk-4.0/gtk.css.bak;
+			fi
+		'';
+		wantedBy = [ "multi-user.target" ];
+	};
 }
